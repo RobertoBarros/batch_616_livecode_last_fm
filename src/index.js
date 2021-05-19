123270a907b356344ff56bede4695c03c65abe55
Roberto Barros <roberto.f.barros@gmail.com>
2021-05-19 16:56:21 -0300

    livecode

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const api_key = "99b6a9d8450ca4686ff1bb85a5c80edc"; // https://www.last.fm/api/authentication
+
+// http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=u2&api_key=99b6a9d8450ca4686ff1bb85a5c80edc&format=json&limit=5
